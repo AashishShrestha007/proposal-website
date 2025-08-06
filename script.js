f8c79a4ab8653b060575185723f155236e262a78
Aashish Shrestha <94103028+AashishShrestha007@users.noreply.github.com>
2025-08-06 22:36:43 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function yesClicked() {
+  document.getElementById("response").innerHTML = "💖 Yes! You made my dream come true! 💍";
+}
+
+function noClicked() {
+  document.getElementById("response").innerHTML = "😢 That breaks my heart... But I’ll always love you.";
+}
